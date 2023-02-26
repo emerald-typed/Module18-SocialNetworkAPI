1f76e2d758be28918be8510da61154dd7473385b
kyle <kylehippledev@gmail.com>
2023-02-26 15:35:39 -0600

    Fixed some bugs now you can add delete and edit a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,7 @@ const { User } = require("../models");
 const userController = {
 
     getAllUsers(req, res) {
-        User.find({})
-        .populate({path: "thoughts",select: "-__v" })
+        User.find()
         .select("-__v")
         .sort({ _id: -1 })
         .then(userData => res.json(userData))
@@ -16,11 +15,10 @@ const userController = {
 
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-        .populate({path: "thoughts",select: "-__v" })
         .select("-__v")
         .then(userData => {
             if(!userData) {
-                res.status(404).json({ message: "Could not find a USER with ${id}" });
+                res.status(404).json({ message: `Could not find a USER with ${params.id}` });
                 return;
             }
             res.json(userData);
@@ -41,19 +39,19 @@ const userController = {
         User.findOneAndUpdate({_id: params.id}, body, { new: true, runValidators: true })
         .then(userData => {
             if(!userData) {
-                res.status(404).json({ message: "Could not find a USER with ${id}" });
+                res.status(404).json({ message: `Could not find a USER with ${params.id}`});
                 return;
             }
             res.json(userData);
         })
         .catch(err => res.status(400).json(err));
     },
-    //delete user
+
     deleteUser({params}, res){
         User.findOneAndDelete({_id: params.id})
         .then(userData => {
             if(!userData) {
-                res.status(404).json({ message: "Could not find a USER with ${id}" });
+                res.status(404).json({ message: `Could not find a USER with ${params.id}` });
                 return;
             }
             res.json(userData);
